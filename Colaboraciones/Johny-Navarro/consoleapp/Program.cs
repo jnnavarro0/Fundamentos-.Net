1d348b8f89b8203cd4fb3c7b3e2600f52f260990
jnnavarro0 <jnnavarro@espe.edu.ec>
2023-06-19 18:27:10 -0500

    Clonando repositorio

---
+++ Colaboraciones/Johny-Navarro/consoleapp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Humanizer;
+
+Console.WriteLine("Por favor ingrese un nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad:");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hola, Mi nombre es {nombre}, soy {cargo} en la Universidad de las Fuerzas Armadas ESPE y actualmente tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años.");
